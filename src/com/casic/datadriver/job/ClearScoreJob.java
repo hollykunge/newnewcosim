79d305243c2fdddef958a44897fdd0e1a75214b3
hollykunge <hollykunge@163.com>
2018-09-25 09:30:26 +0800

    job

---
+++ src/com/casic/datadriver/job/ClearScoreJob.java
@@ -0,0 +1,11 @@
+package com.casic.datadriver.job;
+
+import com.hotent.core.scheduler.BaseJob;
+import org.quartz.JobExecutionContext;
+
+public class ClearScoreJob extends BaseJob {
+    @Override
+    public void executeJob(JobExecutionContext context) throws Exception {
+
+    }
+}
